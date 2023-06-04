ee6da5b23cb900caefe4b47a79d60692d0bb9154
sibi <sibi@ventitechnologies.com>
2023-06-04 12:45:05 +0800

    instance created

--- code/2_hello_triangle_application/2_hello_triangle_application.cpp
+++ code/2_hello_triangle_application/2_hello_triangle_application.cpp
@@ -15,8 +15,37 @@ void HelloTriangleApplication::initWindow()
     window_ = glfwCreateWindow(window_width_, window_height_, "Vulkan", nullptr,
                                nullptr);
 }
+
+void HelloTriangleApplication::createInstance() {
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    createInfo.enabledLayerCount = 0;
+    if (VK_SUCCESS != vkCreateInstance(&createInfo, nullptr, &instance_)) {
+        throw std::runtime_error("failed to create instance!");
+    } else {
+        std::cout << "vkCreateInstance created " << std::endl;
+    }
+}
 void HelloTriangleApplication::initVulkan() {
     std::cout << " Vulkan Initiated" << std::endl;
+    createInstance();
 }
 void HelloTriangleApplication::mainLoop() {
     std::cout << " Vulkan mainLoop started" << std::endl;
--- code/2_hello_triangle_application/2_hello_triangle_application.hpp
+++ code/2_hello_triangle_application/2_hello_triangle_application.hpp
@@ -3,25 +3,28 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
-#include <cstdlib>
 
-class HelloTriangleApplication{
-public:
+class HelloTriangleApplication {
+  public:
     ~HelloTriangleApplication();
-    void run(){
+    void run() {
         initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
-const uint32_t window_width_ = 800;
-const uint32_t window_height_ = 600;
-private:
+    const uint32_t window_width_ = 800;
+    const uint32_t window_height_ = 600;
+
+  private:
     void initWindow();
     void initVulkan();
     void mainLoop();
     void cleanup();
-    GLFWwindow* window_;
+    void createInstance();
+    GLFWwindow *window_;
+    VkInstance instance_;
 };
